298bdafab8e7e101af4a207e4536b16cd30a0542
Arvinder Dhanoa <awsomearvinder@gmail.com>
2020-02-12 10:21:57 -0600

    use more standard formatting

--- src/bridge/keybindings.rs
+++ src/bridge/keybindings.rs
@@ -195,6 +195,10 @@ pub fn append_modifiers(modifiers: ModifiersState, keycode_text: &str, special: 
             "8" => "*".to_string(),
             "9" => "(".to_string(),
             "0" => ")".to_string(),
+            "<" => {
+                special = true;
+                "lt".to_string()
+            }
             other => {
                 special = true;
                 format!("S-{}", other)
@@ -216,9 +220,6 @@ pub fn append_modifiers(modifiers: ModifiersState, keycode_text: &str, special: 
 
     if special {
         result = format!("<{}>", result);
-    }
-    if &result == "<" {
-        result = "<lt>".into();
     }
     result
 }
