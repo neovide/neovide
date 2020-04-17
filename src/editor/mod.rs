df229c602c6c05e5beae0f52650145fe1aff7d15
exoticus <56480961+exoticus@users.noreply.github.com>
2020-04-17 15:22:26 +0200

    more house keeping 🧹

--- src/editor/mod.rs
+++ src/editor/mod.rs
@@ -111,6 +111,7 @@ impl Editor {
 
     pub fn build_draw_commands(&mut self) -> (Vec<DrawCommand>, bool) {
         let mut draw_commands = Vec::new();
+
         for (row_index, row) in self.grid.rows().enumerate() {
             let mut command = None;
 
