3239f13c6826440686148e5c1216d01d4477fce7
AnhQuan Nguyen <j4qfrost@gmail.com>
2020-05-13 17:43:33 -0700

    remove sdl-sys build-dep; segregate windows dep

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() {
-    if cfg!(target_os = "windows") {
+    #[cfg(windows)]
+    {
         let mut res = winres::WindowsResource::new();
         res.set_icon("assets/nvim.ico");
         res.compile().expect("Could not attach exe icon");
