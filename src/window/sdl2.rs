21a24187c4bd8da0a360d97a2a14fadad60e01a9
Jack Frost <j4qfrost@gmail.com>
2020-09-13 21:41:32 +0000

    refactor and fix modifiers

--- src/window/sdl2.rs
+++ src/window/sdl2.rs
@@ -2,16 +2,17 @@ use std::sync::atomic::Ordering;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 
-use log::{debug, error, info, trace};
+use log::{error, info, trace};
 use skulpin::sdl2;
 use skulpin::sdl2::event::{Event, WindowEvent};
+use skulpin::sdl2::keyboard::KeyboardUtil;
 use skulpin::sdl2::keyboard::Keycode;
 use skulpin::sdl2::video::FullscreenType;
+use skulpin::sdl2::video::Window;
 use skulpin::sdl2::EventPump;
-use skulpin::sdl2::Sdl;
 use skulpin::{
     CoordinateSystem, LogicalSize, PhysicalSize, PresentMode, Renderer as SkulpinRenderer,
-    RendererBuilder, Sdl2Window, Window,
+    RendererBuilder, Sdl2Window, Window as OtherWindow,
 };
 
 use crate::bridge::{produce_neovim_keybinding_string, UiCommand, BRIDGE};
@@ -36,8 +37,8 @@ fn windows_fix_dpi() {
 
 fn handle_new_grid_size(new_size: LogicalSize, renderer: &Renderer) {
     if new_size.width > 0 && new_size.height > 0 {
-        let new_width = ((new_size.width + 1) as f32 / renderer.font_width) as u32;
-        let new_height = ((new_size.height + 1) as f32 / renderer.font_height) as u32;
+        let new_width = (new_size.width as f32 / renderer.font_width) as u32;
+        let new_height = (new_size.height as f32 / renderer.font_height) as u32;
         // Add 1 here to make sure resizing doesn't change the grid size on startup
         BRIDGE.queue_command(UiCommand::Resize {
             width: new_width,
@@ -46,19 +47,13 @@ fn handle_new_grid_size(new_size: LogicalSize, renderer: &Renderer) {
     }
 }
 
-struct WindowWrapper {
-    context: Sdl,
-    window: sdl2::video::Window,
-    skulpin_renderer: SkulpinRenderer,
+struct WindowHelper {
     renderer: Renderer,
+    keyboard: Option<KeyboardUtil>,
     mouse_down: bool,
     mouse_position: LogicalSize,
-    title: String,
-    previous_size: LogicalSize,
-    transparency: f32,
-    fullscreen: bool,
-    cached_size: (u32, u32),
-    cached_position: (i32, i32),
+    logical_size: LogicalSize,
+    // previous_position: (i32, i32),
 }
 
 pub fn window_geometry() -> Result<(u64, u64), String> {
@@ -103,147 +98,25 @@ pub fn window_geometry_or_default() -> (u64, u64) {
     window_geometry().unwrap_or(INITIAL_DIMENSIONS)
 }
 
-impl WindowWrapper {
-    pub fn new() -> WindowWrapper {
-        let context = sdl2::init().expect("Failed to initialize sdl2");
-        let video_subsystem = context
-            .video()
-            .expect("Failed to create sdl video subsystem");
-        video_subsystem.text_input().start();
-
+impl WindowHelper {
+    pub fn new() -> WindowHelper {
         let (width, height) = window_geometry_or_default();
-
         let renderer = Renderer::new();
         let logical_size = LogicalSize {
             width: (width as f32 * renderer.font_width) as u32,
-            height: (height as f32 * renderer.font_height + 1.0) as u32,
+            height: (height as f32 * renderer.font_height) as u32,
         };
 
-        #[cfg(target_os = "windows")]
-        windows_fix_dpi();
-        sdl2::hint::set("SDL_MOUSE_FOCUS_CLICKTHROUGH", "1");
-
-        let mut sdl_window = video_subsystem
-            .window("Neovide", logical_size.width, logical_size.height)
-            .position_centered()
-            .allow_highdpi()
-            .resizable()
-            .vulkan()
-            .build()
-            .expect("Failed to create window");
-        info!("window created");
-
-        if std::env::args().any(|arg| arg == "--maximized") {
-            sdl_window.maximize();
-        }
-
-        let skulpin_renderer = {
-            let sdl_window_wrapper = Sdl2Window::new(&sdl_window);
-            RendererBuilder::new()
-                .prefer_integrated_gpu()
-                .use_vulkan_debug_layer(false)
-                .present_mode_priority(vec![PresentMode::Immediate])
-                .coordinate_system(CoordinateSystem::Logical)
-                .build(&sdl_window_wrapper)
-                .expect("Failed to create renderer")
-        };
-
-        info!("renderer created");
-
-        WindowWrapper {
-            context,
-            window: sdl_window,
-            skulpin_renderer,
+        WindowHelper {
             renderer,
+            keyboard: None,
             mouse_down: false,
             mouse_position: LogicalSize {
                 width: 0,
                 height: 0,
             },
-            title: String::from("Neovide"),
-            previous_size: logical_size,
-            transparency: 1.0,
-            fullscreen: false,
-            cached_size: (0, 0),
-            cached_position: (0, 0),
-        }
-    }
-
-    pub fn toggle_fullscreen(&mut self) {
-        if self.fullscreen {
-            if cfg!(target_os = "windows") {
-                unsafe {
-                    let raw_handle = self.window.raw();
-                    sdl2::sys::SDL_SetWindowResizable(raw_handle, sdl2::sys::SDL_bool::SDL_TRUE);
-                }
-            } else {
-                self.window.set_fullscreen(FullscreenType::Off).ok();
-            }
-
-            // Use cached size and position
-            self.window
-                .set_size(self.cached_size.0, self.cached_size.1)
-                .unwrap();
-            self.window.set_position(
-                sdl2::video::WindowPos::Positioned(self.cached_position.0),
-                sdl2::video::WindowPos::Positioned(self.cached_position.1),
-            );
-        } else {
-            self.cached_size = self.window.size();
-            self.cached_position = self.window.position();
-
-            if cfg!(target_os = "windows") {
-                let video_subsystem = self.window.subsystem();
-                if let Ok(rect) = self
-                    .window
-                    .display_index()
-                    .and_then(|index| video_subsystem.display_bounds(index))
-                {
-                    // Set window to fullscreen
-                    unsafe {
-                        let raw_handle = self.window.raw();
-                        sdl2::sys::SDL_SetWindowResizable(
-                            raw_handle,
-                            sdl2::sys::SDL_bool::SDL_FALSE,
-                        );
-                    }
-                    self.window.set_size(rect.width(), rect.height()).unwrap();
-                    self.window.set_position(
-                        sdl2::video::WindowPos::Positioned(rect.x()),
-                        sdl2::video::WindowPos::Positioned(rect.y()),
-                    );
-                }
-            } else {
-                self.window.set_fullscreen(FullscreenType::Desktop).ok();
-            }
-        }
-
-        self.fullscreen = !self.fullscreen;
-    }
-
-    pub fn synchronize_settings(&mut self) {
-        let editor_title = { EDITOR.lock().title.clone() };
-
-        if self.title != editor_title {
-            self.title = editor_title;
-            self.window
-                .set_title(&self.title)
-                .expect("Could not set title");
-        }
-
-        let transparency = { SETTINGS.get::<WindowSettings>().transparency };
-
-        if let Ok(opacity) = self.window.opacity() {
-            if (opacity - transparency).abs() > std::f32::EPSILON {
-                self.window.set_opacity(transparency).ok();
-                self.transparency = transparency;
-            }
-        }
-
-        let fullscreen = { SETTINGS.get::<WindowSettings>().fullscreen };
-
-        if self.fullscreen != fullscreen {
-            self.toggle_fullscreen();
+            logical_size,
+            // previous_position: (0, 0),
         }
     }
 
@@ -252,7 +125,7 @@ impl WindowWrapper {
     }
 
     pub fn handle_keyboard_input(&mut self, keycode: Option<Keycode>, text: Option<String>) {
-        let modifiers = self.context.keyboard().mod_state();
+        let modifiers = self.keyboard.as_ref().unwrap().mod_state();
 
         if keycode.is_some() || text.is_some() {
             trace!(
@@ -269,14 +142,14 @@ impl WindowWrapper {
         }
     }
 
-    pub fn handle_pointer_motion(&mut self, x: i32, y: i32) {
+    pub fn handle_pointer_motion(&mut self, x: i32, y: i32, window: &Window) {
         let previous_position = self.mouse_position;
         let physical_size = PhysicalSize::new(
             (x as f32 / self.renderer.font_width) as u32,
             (y as f32 / self.renderer.font_height) as u32,
         );
 
-        let sdl_window_wrapper = Sdl2Window::new(&self.window);
+        let sdl_window_wrapper = Sdl2Window::new(window);
         self.mouse_position = physical_size.to_logical(sdl_window_wrapper.scale_factor());
         if self.mouse_down && previous_position != self.mouse_position {
             BRIDGE.queue_command(UiCommand::Drag(
@@ -339,7 +212,7 @@ impl WindowWrapper {
         REDRAW_SCHEDULER.queue_next_frame();
     }
 
-    pub fn process_editor_events(&mut self, event_pump: &mut EventPump) {
+    pub fn process_editor_events(&mut self, event_pump: &mut EventPump, window: &mut Window) {
         let mut keyboard_inputs = Vec::new();
         let mut keycode = None;
         let mut keytext = None;
@@ -372,7 +245,7 @@ impl WindowWrapper {
 
                     keytext = Some(text);
                 }
-                Event::MouseMotion { x, y, .. } => self.handle_pointer_motion(x, y),
+                Event::MouseMotion { x, y, .. } => self.handle_pointer_motion(x, y, window),
                 Event::MouseButtonDown { .. } => self.handle_pointer_down(),
                 Event::MouseButtonUp { .. } => self.handle_pointer_up(),
                 Event::MouseWheel { x, y, .. } => self.handle_mouse_wheel(x, y),
@@ -386,6 +259,14 @@ impl WindowWrapper {
                 } => {
                     ignore_text_this_frame = true; // Ignore any text events on the first frame when focus is regained. https://github.com/Kethku/neovide/issues/193
                     self.handle_focus_gained();
+                }
+                Event::Window {
+                    win_event: WindowEvent::Resized(width, height),
+                    ..
+                } => {
+                    let new_size = LogicalSize::new(width as u32, height as u32);
+                    handle_new_grid_size(new_size, &self.renderer);
+                    self.logical_size = new_size;
                 }
                 Event::Window { .. } => REDRAW_SCHEDULER.queue_next_frame(),
                 _ => {}
@@ -401,32 +282,24 @@ impl WindowWrapper {
         }
     }
 
-    pub fn draw_frame(&mut self) -> bool {
+    pub fn should_draw(&self) -> bool {
+        REDRAW_SCHEDULER.should_draw() || SETTINGS.get::<WindowSettings>().no_idle
+    }
+
+    pub fn draw_frame(
+        &mut self,
+        window: &mut Sdl2Window,
+        skulpin_renderer: &mut SkulpinRenderer,
+    ) -> bool {
         if !BRIDGE.running.load(Ordering::Relaxed) {
             return false;
         }
-
-        let sdl_window_wrapper = Sdl2Window::new(&self.window);
-        let new_size = sdl_window_wrapper.logical_size();
-        if self.previous_size != new_size {
-            handle_new_grid_size(new_size, &self.renderer);
-            self.previous_size = new_size;
-        }
-
-        debug!("Render Triggered");
-
-        let current_size = self.previous_size;
-
-        if REDRAW_SCHEDULER.should_draw() || SETTINGS.get::<WindowSettings>().no_idle {
+        if self.should_draw() {
             let renderer = &mut self.renderer;
-            let error = self
-                .skulpin_renderer
-                .draw(&sdl_window_wrapper, |canvas, coordinate_system_helper| {
+            let error = skulpin_renderer
+                .draw(window, |canvas, coordinate_system_helper| {
                     let dt = 1.0 / (SETTINGS.get::<WindowSettings>().refresh_rate as f32);
-
-                    if renderer.draw(canvas, &coordinate_system_helper, dt) {
-                        handle_new_grid_size(current_size, &renderer)
-                    }
+                    renderer.draw(canvas, &coordinate_system_helper, dt);
                 })
                 .is_err();
             if error {
@@ -436,6 +309,73 @@ impl WindowWrapper {
         }
 
         true
+    }
+}
+
+pub fn toggle_fullscreen(window: &mut Window) {
+    match window.fullscreen_state() {
+        FullscreenType::Off => {
+            if cfg!(target_os = "windows") {
+                let video_subsystem = window.subsystem();
+                if let Ok(rect) = window
+                    .display_index()
+                    .and_then(|index| video_subsystem.display_bounds(index))
+                {
+                    // Set window to fullscreen
+                    unsafe {
+                        let raw_handle = window.raw();
+                        sdl2::sys::SDL_SetWindowResizable(
+                            raw_handle,
+                            sdl2::sys::SDL_bool::SDL_FALSE,
+                        );
+                    }
+                    window.set_size(rect.width(), rect.height()).unwrap();
+                    window.set_position(
+                        sdl2::video::WindowPos::Positioned(rect.x()),
+                        sdl2::video::WindowPos::Positioned(rect.y()),
+                    );
+                }
+            } else {
+                window.set_fullscreen(FullscreenType::Desktop).ok();
+            }
+        }
+        _ => {
+            if cfg!(target_os = "windows") {
+                unsafe {
+                    let raw_handle = window.raw();
+                    sdl2::sys::SDL_SetWindowResizable(raw_handle, sdl2::sys::SDL_bool::SDL_TRUE);
+                }
+            } else {
+                window.set_fullscreen(FullscreenType::Off).ok();
+            }
+        }
+    }
+}
+
+pub fn synchronize_settings(window: &mut Window) {
+    let editor_title = { EDITOR.lock().title.clone() };
+
+    if window.title() != editor_title {
+        window
+            .set_title(&editor_title)
+            .expect("Could not set title");
+    }
+
+    if let Ok(opacity) = window.opacity() {
+        let transparency = { SETTINGS.get::<WindowSettings>().transparency };
+        if (opacity - transparency).abs() > std::f32::EPSILON {
+            window.set_opacity(transparency).ok();
+        }
+    }
+
+    let fullscreen_state = if SETTINGS.get::<WindowSettings>().fullscreen {
+        FullscreenType::Desktop
+    } else {
+        FullscreenType::Off
+    };
+    if window.fullscreen_state() != fullscreen_state {
+        println!("{:?}", fullscreen_state);
+        toggle_fullscreen(window);
     }
 }
 
@@ -466,20 +406,56 @@ pub fn initialize_settings() {
 }
 
 pub fn ui_loop() {
-    let mut window = WindowWrapper::new();
+    let mut window_helper = WindowHelper::new();
+    let context = sdl2::init().expect("Failed to initialize sdl2");
+    let video_subsystem = context
+        .video()
+        .expect("Failed to create sdl video subsystem");
+    video_subsystem.text_input().start();
+
+    window_helper.keyboard = Some(context.keyboard());
+
+    #[cfg(target_os = "windows")]
+    windows_fix_dpi();
+    sdl2::hint::set("SDL_MOUSE_FOCUS_CLICKTHROUGH", "1");
+
+    let mut sdl_window = video_subsystem
+        .window(
+            "Neovide",
+            window_helper.logical_size.width,
+            window_helper.logical_size.height,
+        )
+        .position_centered()
+        .allow_highdpi()
+        .resizable()
+        .vulkan()
+        .build()
+        .expect("Failed to create window");
+    info!("window created");
+
+    let mut skulpin_renderer = {
+        let sdl_window_wrapper = Sdl2Window::new(&sdl_window);
+        RendererBuilder::new()
+            .prefer_integrated_gpu()
+            .use_vulkan_debug_layer(false)
+            .present_mode_priority(vec![PresentMode::Immediate])
+            .coordinate_system(CoordinateSystem::Logical)
+            .build(&sdl_window_wrapper)
+            .expect("Failed to create renderer")
+    };
 
     info!("Starting window event loop");
-    let mut event_pump = window
-        .context
+    let mut event_pump = context
         .event_pump()
         .expect("Could not create sdl event pump");
 
     loop {
         let frame_start = Instant::now();
-        window.synchronize_settings();
+        synchronize_settings(&mut sdl_window);
 
-        window.process_editor_events(&mut event_pump);
-        if !window.draw_frame() {
+        window_helper.process_editor_events(&mut event_pump, &mut sdl_window);
+        let mut sdl_window_wrapper = Sdl2Window::new(&sdl_window);
+        if !window_helper.draw_frame(&mut sdl_window_wrapper, &mut skulpin_renderer) {
             break;
         }
 
