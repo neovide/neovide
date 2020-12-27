3d87ae6e0a20bb35061bc9abd8d3148ee4607731
Keith Simmons <keith@the-simmons.net>
2020-12-26 21:38:06 -0800

    parse viewport events

--- src/bridge/events.rs
+++ src/bridge/events.rs
@@ -216,6 +216,13 @@ pub enum RedrawEvent {
         scrolled: bool,
         separator_character: String,
     },
+    WindowViewport {
+        grid: u64,
+        top_line: f64,
+        bottom_line: f64,
+        current_line: f64,
+        current_column: f64,
+    },
     CommandLineShow {
         content: StyledContent,
         position: u64,
@@ -729,6 +736,27 @@ fn parse_msg_set_pos(msg_set_pos_arguments: Vec<Value>) -> Result<RedrawEvent> {
         row: parse_u64(row)?,
         scrolled: parse_bool(scrolled)?,
         separator_character: parse_string(separator_character)?,
+    })
+}
+
+fn parse_win_viewport(win_viewport_arguments: Vec<Value>) -> Result<RedrawEvent> {
+    let values = [
+        Value::Nil,
+        Value::Nil,
+        Value::Nil,
+        Value::Nil,
+        Value::Nil,
+        Value::Nil,
+    ];
+    let [grid, _window, top_line, bottom_line, current_line, current_column] =
+        extract_values(win_viewport_arguments, values)?;
+
+    Ok(RedrawEvent::WindowViewport {
+        grid: parse_u64(grid)?,
+        top_line: parse_f64(top_line)?,
+        bottom_line: parse_f64(bottom_line)?,
+        current_line: parse_f64(current_line)?,
+        current_column: parse_f64(current_column)?,
     })
 }
 
