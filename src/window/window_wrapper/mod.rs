910aa2e044bc93ec3ff2ba41f99dd323b4219edd
shaunsingh <71196912+shaunsingh@users.noreply.github.com>
2021-05-24 20:07:02 -0400

    Update mod.rs

--- src/window/window_wrapper/mod.rs
+++ src/window/window_wrapper/mod.rs
@@ -329,7 +329,7 @@ impl GlutinWindowWrapper {
                         ..
                     },
                 ..
-            } => self.handle_mouse_wheel(0, lpos.y as i32),
+            } => self.handle_mouse_wheel(0, (lpos.y as i32)/10),
             Event::WindowEvent {
                 event:
                     WindowEvent::MouseInput {
