5de9397af5548e2bd64df5f49e68719a37906d68
shaunsingh <71196912+shaunsingh@users.noreply.github.com>
2021-05-24 22:35:55 -0400

    somehow still too fast

--- src/window/window_wrapper/mod.rs
+++ src/window/window_wrapper/mod.rs
@@ -329,7 +329,7 @@ impl GlutinWindowWrapper {
                         ..
                     },
                 ..
-            } => self.handle_mouse_wheel(0.0, (lpos.y as f32)/5000f32),
+            } => self.handle_mouse_wheel(0.0, (lpos.y as f32)/50000f32),
             Event::WindowEvent {
                 event:
                     WindowEvent::MouseInput {
