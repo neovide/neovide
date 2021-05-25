15d6a8336230c43031da10c38372ff07c2eebdb8
shaunsingh <71196912+shaunsingh@users.noreply.github.com>
2021-05-24 22:02:37 -0400

    I completely forgot about how unsigned integers in rust work

--- src/window/window_wrapper/mod.rs
+++ src/window/window_wrapper/mod.rs
@@ -329,7 +329,7 @@ impl GlutinWindowWrapper {
                         ..
                     },
                 ..
-            } => self.handle_mouse_wheel(0, lpos.y as i32),
+            } => self.handle_mouse_wheel(0, (lpos.y as f32)/10f32 as i32;),
             Event::WindowEvent {
                 event:
                     WindowEvent::MouseInput {
