920ea5174398d1045fc36fed73e1643384fc9020
Tim Biermann <tbier@posteo.de>
2021-07-18 15:58:32 +0200

    getting rid of superfluous to_string()

--- src/window/window_wrapper/mod.rs
+++ src/window/window_wrapper/mod.rs
@@ -222,11 +222,11 @@ pub fn start_loop(
         .with_decorations(!SETTINGS.get::<CmdLineSettings>().frameless);
 
     #[cfg(target_os = "linux")]
-    let winit_window_builder = winit_window_builder
-        .with_app_id(SETTINGS.get::<CmdLineSettings>().wayland_app_id.to_string());
+    let winit_window_builder =
+        winit_window_builder.with_app_id(SETTINGS.get::<CmdLineSettings>().wayland_app_id);
     #[cfg(target_os = "linux")]
     let winit_window_builder = winit_window_builder.with_class(
-        SETTINGS.get::<CmdLineSettings>().x11_wm_class.to_string(),
+        SETTINGS.get::<CmdLineSettings>().x11_wm_class,
         "Neovide".to_string(),
     );
 
