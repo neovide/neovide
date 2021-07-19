cfbc436dda1a56f9e578066bdd87e61f1f58debe
Tim Biermann <tbier@posteo.de>
2021-07-19 09:37:43 +0200

    fixed class

--- src/window/window_wrapper/mod.rs
+++ src/window/window_wrapper/mod.rs
@@ -224,10 +224,8 @@ pub fn start_loop(
     #[cfg(target_os = "linux")]
     let winit_window_builder = winit_window_builder
         .with_app_id(SETTINGS.get::<CmdLineSettings>().wayland_app_id)
-        .with_class(
-            SETTINGS.get::<CmdLineSettings>().x11_wm_class,
-            "neovide".to_string(),
-        );
+        .with_class("neovide".to_string(), SETTINGS.get::<CmdLineSettings>().x11_wm_class
+    );
 
     let windowed_context = ContextBuilder::new()
         .with_pixel_format(24, 8)
