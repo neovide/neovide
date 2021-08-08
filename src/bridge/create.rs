f0b40e1a26fca8046387f289859c6164a04e4ef1
KillTheMule <KillTheMule@users.noreply.github.com>
2021-08-08 22:33:46 +0200

    Upgrade to tokio 1.1.1

--- src/bridge/create.rs
+++ src/bridge/create.rs
@@ -14,7 +14,11 @@ use tokio::{
     task::JoinHandle,
 };
 
-use nvim_rs::compat::tokio::TokioAsyncReadCompatExt;
+use tokio_util::compat::{
+  TokioAsyncReadCompatExt,
+};
+
+//use nvim_rs::compat::tokio::TokioAsyncReadCompatExt;
 use nvim_rs::{error::LoopError, neovim::Neovim, Handler};
 
 use crate::bridge::{TxWrapper, WrapTx};
@@ -30,7 +34,7 @@ where
 {
     let stream = TcpStream::connect(addr).await?;
     let (reader, writer) = split(stream);
-    let (neovim, io) = Neovim::<TxWrapper>::new(reader.compat_read(), writer.wrap_tx(), handler);
+    let (neovim, io) = Neovim::<TxWrapper>::new(reader.compat(), writer.wrap_tx(), handler);
     let io_handle = spawn(io);
 
     Ok((neovim, io_handle))
@@ -51,7 +55,7 @@ where
         .stdout
         .take()
         .ok_or_else(|| Error::new(ErrorKind::Other, "Can't open stdout"))?
-        .compat_read();
+        .compat();
     let stdin = child
         .stdin
         .take()
