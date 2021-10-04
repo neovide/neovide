0ef1521a182fd0117714a7fef8023dc47129ab7d
Keith Simmons <keith@the-simmons.net>
2021-10-04 00:25:18 -0700

    compiling but quits on startup

---
+++ src/running_tracker.rs
@@ -0,0 +1,34 @@
+use std::sync::{
+    Arc, 
+    atomic::{
+        AtomicBool,
+        Ordering,
+    },
+};
+
+use log::info;
+
+lazy_static! {
+    pub static ref RUNNING_TRACKER: RunningTracker = RunningTracker::new();
+}
+
+pub struct RunningTracker {
+    running: Arc<AtomicBool>
+}
+
+impl RunningTracker {
+    fn new() -> Self {
+        Self {
+            running: Arc::new(AtomicBool::new(true))
+        }
+    }
+
+    pub fn quit(&self, reason: &str) {
+        self.running.store(false, Ordering::Relaxed);
+        info!("Quit {}", reason);
+    }
+
+    pub fn is_running(&self) -> bool {
+        self.running.load(Ordering::Relaxed)
+    }
+}
