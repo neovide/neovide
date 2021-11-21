551236a29006787544c057ef7212e5812208a60f
Johan Sköld <git@johan.skold.cc>
2021-11-21 04:09:03 -0800

    Redraw window when requested

--- src/window/window_wrapper/mod.rs
+++ src/window/window_wrapper/mod.rs
@@ -164,6 +164,7 @@ impl GlutinWindowWrapper {
                     self.handle_focus_lost();
                 }
             }
+            Event::RedrawRequested(..) |
             Event::WindowEvent { .. } => REDRAW_SCHEDULER.queue_next_frame(),
             _ => {}
         }
