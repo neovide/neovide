b8cf73c80f9759ebc13c6ccdb28fc72a7d0de217
lethanix <louismurguia@protonmail.com>
2021-12-13 08:38:35 -0600

    Adding frame file; forgot to add

---
+++ src/utils/frame.rs
@@ -0,0 +1,26 @@
+// Options for the frame decorations
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Frame {
+    Full,
+    #[cfg(target_os = "macos")]
+    Buttonless,
+    None,
+}
+
+impl Default for Frame {
+    fn default() -> Frame {
+        Frame::Full
+    }
+}
+
+impl Frame {
+    pub fn from_string(decoration: String) -> Frame {
+        match decoration.to_lowercase().as_str() {
+            "full" => Frame::Full,
+            #[cfg(target_os = "macos")]
+            "buttonless" => Frame::Buttonless,
+            "none" => Frame::None,
+            _ => Frame::Full,
+        }
+    }
+}
