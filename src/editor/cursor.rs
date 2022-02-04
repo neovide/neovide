92bc1725f1733547eb0ae25b740425f03f358c2a
Natasha England-Elbro <46329225+0x00002a@users.noreply.github.com>
2022-02-04 20:14:45 +0000

    feat: add support for blend=100 (#1195)
    * feat: add support for blend=100
    
    * feat: add support for blend between 0 and 100 for cursor
    
    * fix: cursor not updating on window switch
    
    * fix: crash on set cursor group
    
    * chore: formatting
    
    * chore: curr -> current

--- src/editor/cursor.rs
+++ src/editor/cursor.rs
@@ -80,6 +80,14 @@ impl Cursor {
             .unwrap_or_else(|| default_colors.foreground.unwrap())
     }
 
+    pub fn alpha(&self) -> u8 {
+        return self
+            .style
+            .as_ref()
+            .map(|s| (255 as f32 * ((100 - s.blend) as f32 / (100.0 as f32))) as u8)
+            .unwrap_or(255);
+    }
+
     pub fn change_mode(&mut self, cursor_mode: &CursorMode, styles: &HashMap<u64, Arc<Style>>) {
         let CursorMode {
             shape,
