7643557a2f0ec7fb6ef0bc003dce64f05f97945f
khjorth <13965958+khjorth@users.noreply.github.com>
2022-09-30 17:28:39 +0200

    Use tokio compat_write as TxWrapper.
    This makes TxWrapper a misnomer. I'll rename it in a follow-up commit to
    facilitate easier reviewing.

--- src/bridge/create.rs
+++ src/bridge/create.rs
@@ -8,31 +8,47 @@ use std::{
 
 use nvim_rs::{error::LoopError, neovim::Neovim, Handler};
 use tokio::{
-    io::split,
+    io::{split, AsyncRead, AsyncWrite},
     net::{TcpStream, ToSocketAddrs},
     process::Command,
     spawn,
     task::JoinHandle,
 };
-use tokio_util::compat::TokioAsyncReadCompatExt;
+use tokio_util::compat::{TokioAsyncReadCompatExt, TokioAsyncWriteCompatExt};
 
-use crate::bridge::{TxWrapper, WrapTx};
+use crate::bridge::TxWrapper;
+
+/// Connect to a neovim instance
+async fn connect<H, R, W>(
+    reader: R,
+    writer: W,
+    handler: H,
+) -> io::Result<(Neovim<H::Writer>, JoinHandle<Result<(), Box<LoopError>>>)>
+where
+    R: AsyncRead + Send + Unpin + 'static,
+    W: AsyncWrite + Send + Unpin + 'static,
+    H: Handler<Writer = TxWrapper>,
+{
+    let (neovim, io) =
+        Neovim::<H::Writer>::new(reader.compat(), Box::new(writer.compat_write()), handler);
+    let io_handle = spawn(io);
+
+    Ok((neovim, io_handle))
+}
 
 /// Connect to a neovim instance via tcp.
 pub async fn new_tcp<A, H>(
     addr: A,
     handler: H,
-) -> io::Result<(Neovim<TxWrapper>, JoinHandle<Result<(), Box<LoopError>>>)>
+) -> io::Result<(Neovim<H::Writer>, JoinHandle<Result<(), Box<LoopError>>>)>
 where
     A: ToSocketAddrs,
     H: Handler<Writer = TxWrapper>,
 {
     let stream = TcpStream::connect(addr).await?;
     let (reader, writer) = split(stream);
-    let (neovim, io) = Neovim::<TxWrapper>::new(reader.compat(), writer.wrap_tx(), handler);
-    let io_handle = spawn(io);
 
-    Ok((neovim, io_handle))
+    connect(reader, writer, handler).await
 }
 
 /// Connect to a neovim instance by spawning a new one
@@ -41,24 +57,19 @@ where
 pub async fn new_child_cmd<H>(
     cmd: &mut Command,
     handler: H,
-) -> io::Result<(Neovim<TxWrapper>, JoinHandle<Result<(), Box<LoopError>>>)>
+) -> io::Result<(Neovim<H::Writer>, JoinHandle<Result<(), Box<LoopError>>>)>
 where
     H: Handler<Writer = TxWrapper>,
 {
     let mut child = cmd.stdin(Stdio::piped()).stdout(Stdio::piped()).spawn()?;
-    let stdout = child
+    let reader = child
         .stdout
         .take()
-        .ok_or_else(|| Error::new(ErrorKind::Other, "Can't open stdout"))?
-        .compat();
-    let stdin = child
+        .ok_or_else(|| Error::new(ErrorKind::Other, "Can't open stdout"))?;
+    let writer = child
         .stdin
         .take()
-        .ok_or_else(|| Error::new(ErrorKind::Other, "Can't open stdin"))?
-        .wrap_tx();
+        .ok_or_else(|| Error::new(ErrorKind::Other, "Can't open stdin"))?;
 
-    let (neovim, io) = Neovim::<TxWrapper>::new(stdout, stdin, handler);
-    let io_handle = spawn(io);
-
-    Ok((neovim, io_handle))
+    connect(reader, writer, handler).await
 }
