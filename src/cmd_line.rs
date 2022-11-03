neovide/neovide

commit 57efa715013b1e9acff65c127419767383742a03

Clean up cmdline handling (#1583)
Use clap's derive macro instead of the builder machinery

src/cmd_line.rs
@@ -1,244 +1,125 @@
+use std::{iter, mem};
+
 use crate::{dimensions::Dimensions, frame::Frame, settings::*};
 
-use clap::{Arg, Command};
+use clap::{ArgAction, Parser};
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Parser)]
+#[command(version, about, long_about = None)]
 pub struct CmdLineSettings {
-    // Pass through arguments
+    /// Files to open (plainly appended to NeoVim args)
+    #[arg(
+        num_args = ..,
+        action = ArgAction::Append,
+    )]
+    pub files_to_open: Vec<String>, // Can't be a PathBuf since shlex can't operate on bytes
+
+    /// Arguments to pass down to NeoVim without interpreting them
+    #[arg(
+        num_args = ..,
+        action = ArgAction::Append,
+        last = true,
+        allow_hyphen_values = true
+    )]
     pub neovim_args: Vec<String>,
-    // Command-line arguments only
+
+    /// The geometry of the window
+    #[arg(long, default_value_t = DEFAULT_WINDOW_GEOMETRY)]
     pub geometry: Dimensions,
+
+    /// If to enable logging to a file in the current directory
+    #[arg(long = "log")]
     pub log_to_file: bool,
-    pub no_fork: bool,
+
+    /// Connect to NeoVim through this remote TCP
+    #[arg(long = "remote-tcp")]
     pub remote_tcp: Option<String>,
+
+    /// Run NeoVim in WSL rather than on the host
+    #[arg(long)]
     pub wsl: bool,
-    // Command-line flags with environment variable fallback
+
+    /// Which window decorations to use (do note that the window might not be resizable
+    /// if this is "none")
+    #[arg(long, env = "NEOVIDE_FRAME", default_value_t = Frame::default())]
     pub frame: Frame,
+
+    /// Maximize the window on startup (not equivalent to fullscreen)
+    #[arg(long, env = "NEOVIDE_MAXIMIZED")]
     pub maximized: bool,
+
+    /// Enable the Multigrid extension (enables smooth scrolling and floating blur)
+    #[arg(long = "multigrid", env = "NEOVIDE_MULTIGRID")]
     pub multi_grid: bool,
+
+    /// Instead of spawning a child process and leaking it, be "blocking" and let the shell persist
+    /// as parent process
+    #[arg(long = "nofork")]
+    pub no_fork: bool,
+
+    /// Render every frame, takes more power and CPU time but possibly helps with frame timing
+    /// issues
+    #[arg(long = "noidle", env = "NEOVIDE_NO_IDLE")]
     pub no_idle: bool,
-    pub srgb: bool,
-    // Command-line arguments with environment variable fallback
-    pub neovim_bin: Option<String>,
-    pub wayland_app_id: String,
-    pub x11_wm_class: String,
-    pub x11_wm_class_instance: String,
-    // Disable open multiple files as tabs
+
+    /// Disable opening multiple files supplied in tabs (they're still buffers)
+    #[arg(long = "notabs")]
     pub no_tabs: bool,
+
+    /// Do not request sRGB when initializing the window, may help with GPUs with weird pixel
+    /// formats
+    #[arg(long = "nosrgb", env = "NEOVIDE_NO_SRGB", action = ArgAction::SetFalse)]
+    pub srgb: bool,
+
+    /// Which NeoVim binary to invoke headlessly instead of `nvim` found on $PATH
+    #[arg(long = "neovim-bin", env = "NEOVIM_BIN")]
+    pub neovim_bin: Option<String>,
+
+    /// The app ID to show to the compositor (Wayland only, useful for setting WM rules)
+    #[arg(
+        long = "wayland_app_id",
+        env = "NEOVIDE_APP_ID",
+        default_value = "neovide"
+    )]
+    pub wayland_app_id: String,
+
+    /// The class part of the X11 WM_CLASS property (X only, useful for setting WM rules)
+    #[arg(
+        long = "x11-wm-class",
+        env = "NEOVIDE_WM_CLASS",
+        default_value = "neovide"
+    )]
+    pub x11_wm_class: String,
+
+    /// The instance part of the X11 WM_CLASS property (X only, useful for setting WM rules)
+    #[arg(
+        long = "x11-wm-class-instance",
+        env = "NEOVIDE_WM_CLASS_INSTANCE",
+        default_value = "neovide"
+    )]
+    pub x11_wm_class_instance: String,
 }
 
 impl Default for CmdLineSettings {
     fn default() -> Self {
-        Self {
-            // Pass through arguments
-            neovim_args: vec![],
-            // Command-line arguments only
-            geometry: DEFAULT_WINDOW_GEOMETRY,
-            log_to_file: false,
-            no_fork: false,
-            remote_tcp: None,
-            wsl: false,
-            // Command-line flags with environment variable fallback
-            frame: Frame::Full,
-            maximized: false,
-            multi_grid: false,
-            no_idle: false,
-            srgb: true,
-            // Command-line arguments with environment variable fallback
-            neovim_bin: None,
-            wayland_app_id: String::new(),
-            x11_wm_class_instance: String::new(),
-            x11_wm_class: String::new(),
-            // Disable open multiple files as tabs
-            no_tabs: false,
-        }
+        Self::parse_from(iter::empty::<String>())
     }
 }
 
 pub fn handle_command_line_arguments(args: Vec<String>) -> Result<(), String> {
-    let clapp = Command::new("Neovide")
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about(crate_description!())
-        // Pass through arguments
-        .arg(
-            Arg::new("files_to_open")
-                .multiple_values(true)
-                .takes_value(true)
-                .help("Files to open"),
-        )
-        .arg(
-            Arg::new("neovim_args")
-                .multiple_values(true)
-                .takes_value(true)
-                .last(true)
-                .help("Specify Arguments to pass down to neovim"),
-        )
-        // Command-line arguments only
-        .arg(
-            Arg::new("geometry")
-                .long("geometry")
-                .takes_value(true)
-                .help("Specify the Geometry of the window"),
-        )
-        .arg(
-            Arg::new("log_to_file")
-                .long("log")
-                .help("Log to a file"),
-        )
-        .arg(
-            Arg::new("nofork")
-                .long("nofork")
-                .help(r#"Instead of spawning a child process and leaking it, be "blocking" and have the shell directly as parent process"#),
-        )
-        .arg(
-            Arg::new("no_tabs")
-                .long("notabs")
-                .help("Disable open multiple files as tabs"),
-        )
-        .arg(
-            Arg::new("remote_tcp")
-                .long("remote-tcp")
-                .takes_value(true)
-                .help("Connect to Remote TCP"),
-        )
-        .arg(
-            Arg::new("wsl")
-                .long("wsl")
-                .help("Run in WSL")
-        )
-        // Command-line flags with environment variable fallback
-        .arg(
-            Arg::new("frame")
-            .long("frame")
-            .takes_value(true)
-            .help("Configure the window frame. NOTE: Window might not be resizable if setting is None.")
-        )
-        .arg(
-            Arg::new("maximized")
-                .long("maximized")
-                .help("Maximize the window"),
-        )
-        .arg(
-            Arg::new("multi_grid")
-                .long("multigrid")
-                .help("Enable Multigrid"),
-        )
-        .arg(
-            Arg::new("noidle")
-                .long("noidle")
-                .help("Render every frame. Takes more power and cpu time but possibly fixes animation issues"),
-        )
-        .arg(
-            Arg::new("nosrgb")
-                .long("nosrgb")
-                .help("Do not use standard color space to initialize the window. Swapping this variable sometimes fixes issues on startup"),
-        )
-        // Command-line arguments with environment variable fallback
-        .arg(
-            Arg::new("neovim_bin")
-                .long("neovim-bin")
-                .takes_value(true)
-                .help("Specify path to neovim"),
-        )
-        .arg(
-            Arg::new("wayland_app_id")
-                .long("wayland-app-id")
-                .takes_value(true)
-                .help("Specify an App ID for Wayland"),
-        )
-        .arg(
-            Arg::new("x11_wm_class_instance")
-                .long("x11-wm-class-instance")
-                .takes_value(true)
-                .help("Specify the instance part of the X11 WM_CLASS property"),
-        )
-        .arg(
-            Arg::new("x11_wm_class")
-                .long("x11-wm-class")
-                .takes_value(true)
-                .help("Specify the class part of the X11 WM_CLASS property"),
-        );
+    let mut cmdline = CmdLineSettings::parse_from(args);
 
-    let matches = clapp.get_matches_from(args);
-    let mut neovim_args: Vec<String> = matches
-        .values_of("neovim_args")
-        .map(|opt| opt.map(|v| v.to_owned()).collect())
-        .unwrap_or_default();
+    // The neovim_args in cmdline are unprocessed, actually add options to it
+    let maybe_tab_flag = (!cmdline.no_tabs).then(|| "-p".to_string());
 
-    let files_to_open: Vec<String> = matches
-        .values_of("files_to_open")
-        .map(|opt| opt.map(String::from).collect())
-        .unwrap_or_default();
+    cmdline.neovim_args = maybe_tab_flag
+        .into_iter()
+        .chain(mem::take(&mut cmdline.files_to_open))
+        .chain(cmdline.neovim_args)
+        .map(|arg| shlex::quote(&arg).into_owned())
+        .collect();
 
-    if files_to_open.len() > 1
-        && !neovim_args.contains(&String::from("-p"))
-        && !matches.is_present("no_tabs")
-    {
-        neovim_args.push("-p".to_owned());
-    }
-
-    if cfg!(target_os = "macos") {
-        // escape filepath which contain spaces
-        neovim_args.extend(
-            files_to_open
-                .iter()
-                .map(|file| shlex::quote(file).into_owned()),
-        );
-    } else {
-        neovim_args.extend::<Vec<String>>(files_to_open);
-    }
-
-    /*
-     * Integrate Environment Variables as Defaults to the command-line ones.
-     *
-     * If the command-line argument is not set, the environment variable is used.
-     */
-    SETTINGS.set::<CmdLineSettings>(&CmdLineSettings {
-        // Pass through arguments
-        neovim_args,
-        // Command-line arguments only
-        geometry: parse_window_geometry(matches.value_of("geometry").map(|i| i.to_owned()))?,
-        log_to_file: matches.is_present("log_to_file"),
-        no_fork: matches.is_present("nofork"),
-        remote_tcp: matches.value_of("remote_tcp").map(|i| i.to_owned()),
-        wsl: matches.is_present("wsl"),
-        // Command-line flags with environment variable fallback
-        frame: match matches.value_of("frame") {
-            Some(val) => Frame::from_string(val.to_string()),
-            None => match std::env::var("NEOVIDE_FRAME") {
-                Ok(f) => Frame::from_string(f),
-                Err(_) => Frame::Full,
-            },
-        },
-        maximized: matches.is_present("maximized") || std::env::var("NEOVIDE_MAXIMIZED").is_ok(),
-        multi_grid: matches.is_present("multi_grid") || std::env::var("NEOVIDE_MULTIGRID").is_ok(),
-        no_idle: matches.is_present("noidle") || std::env::var("NEOVIDE_NO_IDLE").is_ok(),
-        // Srgb is enabled by default, so set it to false if nosrgb or NOEVIDE_NO_SRGB is set
-        srgb: !(matches.is_present("nosrgb") || std::env::var("NEOVIDE_NO_SRGB").is_ok()),
-        // Command-line arguments with environment variable fallback
-        neovim_bin: matches
-            .value_of("neovim_bin")
-            .map(|v| v.to_owned())
-            .or_else(|| std::env::var("NEOVIM_BIN").ok()),
-        wayland_app_id: matches
-            .value_of("wayland_app_id")
-            .map(|v| v.to_owned())
-            .or_else(|| std::env::var("NEOVIDE_APP_ID").ok())
-            .unwrap_or_else(|| "neovide".to_owned()),
-        x11_wm_class_instance: matches
-            .value_of("x11_wm_class_instance")
-            .map(|v| v.to_owned())
-            .or_else(|| std::env::var("NEOVIDE_X11_WM_CLASS_INSTANCE").ok())
-            .unwrap_or_else(|| "neovide".to_owned()),
-        x11_wm_class: matches
-            .value_of("x11_wm_class")
-            .map(|v| v.to_owned())
-            .or_else(|| std::env::var("NEOVIDE_X11_WM_CLASS").ok())
-            .unwrap_or_else(|| "neovide".to_owned()),
-        // Disable open multiple files as tabs
-        no_tabs: matches.is_present("no_tabs") || std::env::var("NEOVIDE_NO_TABS").is_ok(),
-    });
+    SETTINGS.set::<CmdLineSettings>(&cmdline);
     Ok(())
 }
 
@@ -258,7 +139,7 @@ mod tests {
 
     #[test]
     fn test_neovim_passthrough() {
-        let args: Vec<String> = vec!["neovide", "--", "--clean"]
+        let args: Vec<String> = vec!["neovide", "--notabs", "--", "--clean"]
             .iter()
             .map(|s| s.to_string())
             .collect();
@@ -273,7 +154,7 @@ mod tests {
 
     #[test]
     fn test_files_to_open() {
-        let args: Vec<String> = vec!["neovide", "./foo.txt", "./bar.md"]
+        let args: Vec<String> = vec!["neovide", "./foo.txt", "--notabs", "./bar.md"]
             .iter()
             .map(|s| s.to_string())
             .collect();
@@ -282,7 +163,7 @@ mod tests {
         handle_command_line_arguments(args).expect("Could not parse arguments");
         assert_eq!(
             SETTINGS.get::<CmdLineSettings>().neovim_args,
-            vec!["-p", "./foo.txt", "./bar.md"]
+            vec!["./foo.txt", "./bar.md"]
         );
     }
 
@@ -304,28 +185,22 @@ mod tests {
         handle_command_line_arguments(args).expect("Could not parse arguments");
         assert_eq!(
             SETTINGS.get::<CmdLineSettings>().neovim_args,
-            vec!["--clean", "./foo.txt", "./bar.md"]
+            vec!["./foo.txt", "./bar.md", "--clean"]
         );
     }
 
     #[test]
     fn test_files_to_open_with_flag() {
-        let args: Vec<String> = vec![
-            "neovide",
-            "--notabs",
-            "./foo.txt",
-            "./bar.md",
-            "--geometry=42x24",
-        ]
-        .iter()
-        .map(|s| s.to_string())
-        .collect();
+        let args: Vec<String> = vec!["neovide", "./foo.txt", "./bar.md", "--geometry=42x24"]
+            .iter()
+            .map(|s| s.to_string())
+            .collect();
 
         let _accessing_settings = ACCESSING_SETTINGS.lock().unwrap();
         handle_command_line_arguments(args).expect("Could not parse arguments");
         assert_eq!(
             SETTINGS.get::<CmdLineSettings>().neovim_args,
-            vec!["./foo.txt", "./bar.md"]
+            vec!["-p", "./foo.txt", "./bar.md"]
         );
 
         assert_eq!(

src/dimensions.rs
@@ -1,13 +1,32 @@
-use std::ops::{Div, Mul};
+use std::{
+    fmt::Display,
+    ops::{Div, Mul},
+    str::FromStr,
+};
 
 use glutin::dpi::PhysicalSize;
 use serde::{Deserialize, Serialize};
+
+use crate::settings;
 
 // Maybe this should be independent from serialization?
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Dimensions {
     pub width: u64,
     pub height: u64,
+}
+
+impl FromStr for Dimensions {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        settings::parse_window_geometry(Some(s.to_string()))
+    }
+}
+
+impl Display for Dimensions {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}x{}", self.width, self.height)
+    }
 }
 
 macro_rules! impl_from_tuple_to_dimensions {

Cargo.toml
@@ -18,7 +18,7 @@ embed-fonts = []
 copypasta = "0.8.1"
 async-trait = "0.1.53"
 cfg-if = "1.0.0"
-clap = { version = "3.1.9", features = ["cargo"] }
+clap = { version = "4.0.8", features = ["cargo", "derive", "env"] }
 csscolorparser = "0.6.2"
 derive-new = "0.5.9"
 dirs = "4.0.0"

src/frame.rs
@@ -1,3 +1,7 @@
+use core::fmt;
+
+use clap::{builder::PossibleValue, ValueEnum};
+
 // Options for the frame decorations
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Frame {
@@ -9,22 +13,46 @@ pub enum Frame {
     None,
 }
 
+impl Frame {
+    fn to_static_str(&self) -> &'static str {
+        match self {
+            Frame::Full => "full",
+
+            #[cfg(target_os = "macos")]
+            Frame::Transparent => "transparent",
+            #[cfg(target_os = "macos")]
+            Frame::Buttonless => "buttonless",
+
+            Frame::None => "none",
+        }
+    }
+}
+
 impl Default for Frame {
     fn default() -> Frame {
         Frame::Full
     }
 }
 
-impl Frame {
-    pub fn from_string(decoration: String) -> Frame {
-        match decoration.to_lowercase().as_str() {
-            "full" => Frame::Full,
-            #[cfg(target_os = "macos")]
-            "transparent" => Frame::Transparent,
-            #[cfg(target_os = "macos")]
-            "buttonless" => Frame::Buttonless,
-            "none" => Frame::None,
-            _ => Frame::Full,
-        }
+impl ValueEnum for Frame {
+    fn value_variants<'a>() -> &'a [Self] {
+        #[cfg(target_os = "macos")]
+        let variants = { &[Self::Full, Self::Transparent, Self::Buttonless, Self::None] };
+        #[cfg(not(target_os = "macos"))]
+        let variants = { &[Self::Full, Self::None] };
+
+        variants
+    }
+
+    fn to_possible_value(&self) -> Option<PossibleValue> {
+        let value = self.to_static_str();
+
+        Some(PossibleValue::new(value))
+    }
+}
+
+impl fmt::Display for Frame {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.to_static_str())
     }
 }

commit d35b366d21f9f30c97b37f8f7f8ed6f1be2690a2

Fix default window geometry to use the last saved value (broken in #1583) (#1616)

src/cmd_line.rs
@@ -24,7 +24,7 @@ pub struct CmdLineSettings {
     pub neovim_args: Vec<String>,
 
     /// The geometry of the window
-    #[arg(long, default_value_t = DEFAULT_WINDOW_GEOMETRY)]
+    #[arg(long, default_value_t = last_window_geometry())]
     pub geometry: Dimensions,
 
     /// If to enable logging to a file in the current directory
@@ -41,7 +41,7 @@ pub struct CmdLineSettings {
 
     /// Which window decorations to use (do note that the window might not be resizable
     /// if this is "none")
-    #[arg(long, env = "NEOVIDE_FRAME", default_value_t = Frame::default())]
+    #[arg(long, env = "NEOVIDE_FRAME", default_value_t)]
     pub frame: Frame,
 
     /// Maximize the window on startup (not equivalent to fullscreen)

src/dimensions.rs
@@ -16,10 +16,16 @@ pub struct Dimensions {
     pub height: u64,
 }
 
+impl Default for Dimensions {
+    fn default() -> Self {
+        settings::DEFAULT_WINDOW_GEOMETRY
+    }
+}
+
 impl FromStr for Dimensions {
     type Err = String;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        settings::parse_window_geometry(Some(s.to_string()))
+        settings::parse_window_geometry(s)
     }
 }
 

src/settings/mod.rs
@@ -14,7 +14,7 @@ use std::{
 use crate::{bridge::TxWrapper, error_handling::ResultPanicExplanation};
 pub use from_value::ParseFromValue;
 pub use window_geometry::{
-    load_last_window_settings, parse_window_geometry, save_window_geometry,
+    last_window_geometry, load_last_window_settings, parse_window_geometry, save_window_geometry,
     PersistentWindowSettings, DEFAULT_WINDOW_GEOMETRY,
 };
 

src/settings/window_geometry.rs
@@ -18,13 +18,9 @@ pub enum PersistentWindowSettings {
     Windowed {
         #[serde(default)]
         position: PhysicalPosition<i32>,
-        #[serde(default = "default_size")]
+        #[serde(default)]
         size: Dimensions,
     },
-}
-
-fn default_size() -> Dimensions {
-    DEFAULT_WINDOW_GEOMETRY
 }
 
 #[derive(Serialize, Deserialize)]
@@ -71,6 +67,18 @@ pub fn load_last_window_settings() -> Result<PersistentWindowSettings, String> {
     Ok(loaded_settings)
 }
 
+pub fn last_window_geometry() -> Dimensions {
+    load_last_window_settings()
+        .and_then(|window_settings| {
+            if let PersistentWindowSettings::Windowed { size, .. } = window_settings {
+                Ok(size)
+            } else {
+                Err(String::from("Window was maximized"))
+            }
+        })
+        .unwrap_or(DEFAULT_WINDOW_GEOMETRY)
+}
+
 pub fn save_window_geometry(
     maximized: bool,
     grid_size: Option<Dimensions>,
@@ -108,45 +116,33 @@ pub fn save_window_geometry(
     std::fs::write(settings_path, json).unwrap();
 }
 
-pub fn parse_window_geometry(geometry: Option<String>) -> Result<Dimensions, String> {
-    let saved_window_size = load_last_window_settings()
-        .and_then(|window_settings| {
-            if let PersistentWindowSettings::Windowed { size, .. } = window_settings {
-                Ok(size)
+pub fn parse_window_geometry(input: &str) -> Result<Dimensions, String> {
+    let invalid_parse_err = format!(
+        "Invalid geometry: {}\nValid format: <width>x<height>",
+        input
+    );
+
+    input
+        .split('x')
+        .map(|dimension| {
+            dimension
+                .parse::<u64>()
+                .map_err(|_| invalid_parse_err.as_str())
+                .and_then(|dimension| {
+                    if dimension > 0 {
+                        Ok(dimension)
+                    } else {
+                        Err("Invalid geometry: Window dimensions should be greater than 0.")
+                    }
+                })
+        })
+        .collect::<Result<Vec<_>, &str>>()
+        .and_then(|dimensions| {
+            if let [width, height] = dimensions[..] {
+                Ok(Dimensions { width, height })
             } else {
-                Err(String::from("Window was maximized"))
+                Err(invalid_parse_err.as_str())
             }
         })
-        .or::<String>(Ok(DEFAULT_WINDOW_GEOMETRY));
-
-    geometry.map_or(saved_window_size, |input| {
-        let invalid_parse_err = format!(
-            "Invalid geometry: {}\nValid format: <width>x<height>",
-            input
-        );
-
-        input
-            .split('x')
-            .map(|dimension| {
-                dimension
-                    .parse::<u64>()
-                    .map_err(|_| invalid_parse_err.as_str())
-                    .and_then(|dimension| {
-                        if dimension > 0 {
-                            Ok(dimension)
-                        } else {
-                            Err("Invalid geometry: Window dimensions should be greater than 0.")
-                        }
-                    })
-            })
-            .collect::<Result<Vec<_>, &str>>()
-            .and_then(|dimensions| {
-                if let [width, height] = dimensions[..] {
-                    Ok(Dimensions { width, height })
-                } else {
-                    Err(invalid_parse_err.as_str())
-                }
-            })
-            .map_err(|msg| msg.to_owned())
-    })
+        .map_err(|msg| msg.to_owned())
 }