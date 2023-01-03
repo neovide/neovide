01c581ad7e5206f53177400882db28668bee84bf
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-01-03 09:25:54 +0100

    Fix typo in variable

--- src/renderer/profiler.rs
+++ src/renderer/profiler.rs
@@ -50,11 +50,11 @@ impl Profiler {
         // Draw FPS
         let color = Color::from_argb(255, 0, 255, 0);
         paint.set_color(color);
-        let mut text_postion = self.position;
-        text_postion.y += self.font.skia_font.size();
+        let mut text_position = self.position;
+        text_position.y += self.font.skia_font.size();
         root_canvas.draw_str(
             format!("{:.0}FPS", 1.0 / dt.max(f32::EPSILON)),
-            text_postion,
+            text_position,
             &self.font.skia_font,
             &paint,
         );
