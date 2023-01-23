0af33fb1966fa9f519c8fabfbdd7097c59362600
Fred Sundvik <fsundvik@gmail.com>
2023-01-23 17:45:20 +0200

    D3D tracy profiling

--- src/profiling/mod.rs
+++ src/profiling/mod.rs
@@ -3,6 +3,8 @@ mod profiling_disabled;
 #[cfg(feature = "profiling")]
 mod profiling_enabled;
 
+#[cfg(all(feature = "gpu_profiling", target_os = "windows"))]
+pub mod d3d;
 #[cfg(feature = "gpu_profiling")]
 pub mod opengl;
 
