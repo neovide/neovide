42ab9d4329945c18916cd78aaab5bcbb1f883ee2
Hawtian <twistoy.wang@gmail.com>
2023-08-10 18:10:36 +0800

    update ParseFromValue trait

--- src/settings/from_value.rs
+++ src/settings/from_value.rs
@@ -1,147 +1,174 @@
 use std::collections::HashMap;
 
 use super::Value;
-use log::error;
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum ParseValueError {
+    TypeMismatch {
+        expect: &'static str,
+        actual: &'static str,
+    },
+}
+
+impl std::fmt::Display for ParseValueError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ParseValueError::TypeMismatch { expect, actual } => {
+                write!(f, "Type mismatch: expect {}, actual {}", expect, actual)
+            }
+        }
+    }
+}
 
 // Trait to allow for conversion from rmpv::Value to any other data type.
 // Note: Feel free to implement this trait for custom types in each subsystem.
 // The reverse conversion (MyType->Value) can be performed by implementing `From<MyType> for Value`
-pub trait ParseFromValue {
-    fn parse_from_value(&mut self, value: &Value) -> bool;
+pub trait ParseFromValue: Sized {
+    fn parse_from_value(value: &Value) -> Result<Self, ParseValueError>;
 }
 
 // FromValue implementations for most typical types
 impl ParseFromValue for f32 {
-    fn parse_from_value(&mut self, value: &Value) -> bool {
-        if value.is_f64() {
-            *self = value.as_f64().unwrap() as f32;
-            true
-        } else if value.is_i64() {
-            *self = value.as_i64().unwrap() as f32;
-            true
-        } else if value.is_u64() {
-            *self = value.as_u64().unwrap() as f32;
-            true
-        } else {
-            error!("Setting expected an f32, but received {:?}", value);
-            false
-        }
+    fn parse_from_value(value: &Value) -> Result<f32, ParseValueError> {
+        value
+            .as_f64()
+            .map(|v| v as f32)
+            .or_else(|| value.as_i64().map(|v| v as f32))
+            .or_else(|| value.as_u64().map(|v| v as f32))
+            .ok_or_else(|| ParseValueError::TypeMismatch {
+                expect: "f32",
+                actual: value_type_name(value),
+            })
     }
 }
 
 impl ParseFromValue for u64 {
-    fn parse_from_value(&mut self, value: &Value) -> bool {
-        if value.is_u64() {
-            *self = value.as_u64().unwrap();
-            true
-        } else {
-            error!("Setting expected a u64, but received {:?}", value);
-            false
-        }
+    fn parse_from_value(value: &Value) -> Result<u64, ParseValueError> {
+        value.as_u64().ok_or_else(|| ParseValueError::TypeMismatch {
+            expect: "u64",
+            actual: value_type_name(value),
+        })
     }
 }
 
 impl ParseFromValue for u32 {
-    fn parse_from_value(&mut self, value: &Value) -> bool {
-        if value.is_u64() {
-            *self = value.as_u64().unwrap() as u32;
-            true
-        } else {
-            error!("Setting expected a u32, but received {:?}", value);
-            false
-        }
+    fn parse_from_value(value: &Value) -> Result<u32, ParseValueError> {
+        value
+            .as_u64()
+            .ok_or_else(|| ParseValueError::TypeMismatch {
+                expect: "u32",
+                actual: value_type_name(value),
+            })
+            .map(|v| v as u32)
     }
 }
 
 impl ParseFromValue for i32 {
-    fn parse_from_value(&mut self, value: &Value) -> bool {
-        if value.is_i64() {
-            *self = value.as_i64().unwrap() as i32;
-            true
-        } else {
-            error!("Setting expected an i32, but received {:?}", value);
-            false
-        }
+    fn parse_from_value(value: &Value) -> Result<i32, ParseValueError> {
+        value
+            .as_u64()
+            .ok_or_else(|| ParseValueError::TypeMismatch {
+                expect: "i32",
+                actual: value_type_name(value),
+            })
+            .map(|v| v as i32)
     }
 }
 
 impl ParseFromValue for String {
-    fn parse_from_value(&mut self, value: &Value) -> bool {
-        if value.is_str() {
-            *self = String::from(value.as_str().unwrap());
-            true
-        } else {
-            error!("Setting expected a string, but received {:?}", value);
-            false
-        }
+    fn parse_from_value(value: &Value) -> Result<String, ParseValueError> {
+        value
+            .as_str()
+            .map(|v| String::from(v))
+            .ok_or_else(|| ParseValueError::TypeMismatch {
+                expect: "String",
+                actual: value_type_name(value),
+            })
     }
 }
 
 impl ParseFromValue for bool {
-    fn parse_from_value(&mut self, value: &Value) -> bool {
-        if value.is_bool() {
-            *self = value.as_bool().unwrap();
-            true
-        } else if value.is_u64() {
-            *self = value.as_u64().unwrap() != 0;
-            true
-        } else {
-            error!("Setting expected a bool or 0/1, but received {:?}", value);
-            false
-        }
+    fn parse_from_value(value: &Value) -> Result<bool, ParseValueError> {
+        value
+            .as_bool()
+            .or_else(|| value.as_u64().map(|v| v != 0))
+            .ok_or_else(|| ParseValueError::TypeMismatch {
+                expect: "bool",
+                actual: value_type_name(value),
+            })
     }
 }
 
 impl<T, U> ParseFromValue for HashMap<T, U>
 where
-    T: Default + ParseFromValue + Eq + std::hash::Hash,
-    U: Default + ParseFromValue,
+    T: ParseFromValue + Eq + std::hash::Hash,
+    U: ParseFromValue,
 {
-    fn parse_from_value(&mut self, value: &Value) -> bool {
-        if value.is_map() {
-            let mut new_map = HashMap::<T, U>::new();
-            let kvs = value.as_map().unwrap();
-            for (k, v) in kvs {
-                let mut key = T::default();
-                if !key.parse_from_value(k) {
-                    return false;
+    fn parse_from_value(value: &Value) -> Result<Self, ParseValueError> {
+        Ok(value
+            .as_map()
+            .map(|kvs| {
+                let mut new_map = HashMap::<T, U>::new();
+                let kvs = value.as_map().unwrap();
+                for (k, v) in kvs {
+                    let key = T::parse_from_value(value)?;
+                    let value = U::parse_from_value(value)?;
+                    new_map.insert(key, value);
                 }
-                let mut value = U::default();
-                if !value.parse_from_value(v) {
-                    return false;
-                }
-                new_map.insert(key, value);
-            }
-            *self = new_map;
-            true
-        } else {
-            error!("Setting expected a map, but received {:?}", value);
-            false
-        }
+                Ok(new_map)
+            })
+            .ok_or_else(|| ParseValueError::TypeMismatch {
+                expect: "map",
+                actual: value_type_name(value),
+            })??)
     }
 }
 
 impl<T> ParseFromValue for Vec<T>
 where
-    T: Default + ParseFromValue,
+    T: ParseFromValue,
 {
-    fn parse_from_value(&mut self, value: &Value) -> bool {
-        if value.is_array() {
-            let mut new_vec = Vec::<T>::new();
-            let arr = value.as_array().unwrap();
-            for v in arr {
-                let mut value = T::default();
-                if !value.parse_from_value(v) {
-                    return false;
+    fn parse_from_value(value: &Value) -> Result<Self, ParseValueError> {
+        Ok(value
+            .as_array()
+            .map(|arr| {
+                let mut new_vec = Vec::<T>::new();
+                let arr = value.as_array().unwrap();
+                for v in arr {
+                    new_vec.push(T::parse_from_value(value)?);
                 }
-                new_vec.push(value);
-            }
-            *self = new_vec;
-            true
-        } else {
-            error!("Setting expected an array, but received {:?}", value);
-            false
-        }
+                Ok(new_vec)
+            })
+            .ok_or_else(|| ParseValueError::TypeMismatch {
+                expect: "array",
+                actual: value_type_name(value),
+            })??)
+    }
+}
+
+fn value_type_name(value: &Value) -> &'static str {
+    if value.is_nil() {
+        "nil"
+    } else if value.is_bool() {
+        "bool"
+    } else if value.is_i64() {
+        "i64"
+    } else if value.is_u64() {
+        "u64"
+    } else if value.is_f64() {
+        "f64"
+    } else if value.is_str() {
+        "str"
+    } else if value.is_array() {
+        "array"
+    } else if value.is_map() {
+        "map"
+    } else if value.is_bin() {
+        "bin"
+    } else if value.is_ext() {
+        "ext"
+    } else {
+        "unknown"
     }
 }
 
@@ -149,6 +176,17 @@ where
 mod tests {
 
     use super::*;
+
+    macro_rules! assert_parse {
+        ($tp:ty, $value:expr, $exp:expr) => {{
+            let v0 = <$tp>::parse_from_value($value);
+            let expect_value: Result<_, ParseValueError> = ($exp).clone();
+            assert_eq!(
+                v0, $exp,
+                "v0 should equal {expect_value:?} but is actually {v0:?}"
+            )
+        }};
+    }
 
     #[test]
     fn test_parse_from_value_f32() {
@@ -160,16 +198,19 @@ mod tests {
         let v2p = -1.0;
         let v3p = std::u64::MAX as f32;
 
-        v0.parse_from_value(&v1);
-        assert_eq!(v0, v1p, "v0 should equal {v1p} but is actually {v0}");
-        v0.parse_from_value(&v2);
-        assert_eq!(v0, v2p, "v0 should equal {v2p} but is actually {v0}");
-        v0.parse_from_value(&v3);
-        assert_eq!(v0, v3p, "v0 should equal {v3p} but is actually {v0}");
+        assert_parse!(f32, &v1, Ok(v1p));
+        assert_parse!(f32, &v2, Ok(v2p));
+        assert_parse!(f32, &v3, Ok(v3p));
 
         // This is a noop and prints an error
-        v0.parse_from_value(&Value::from("asd"));
-        assert_eq!(v0, v3p, "v0 should equal {v3p} but is actually {v0}");
+        assert_eq!(
+            f32::parse_from_value(&Value::from("asd")),
+            Err(ParseValueError::TypeMismatch {
+                expect: "f32",
+                actual: "str",
+            }),
+            "parse should report error but not"
+        );
     }
 
     #[test]
