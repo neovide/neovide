2c5969f94c5d807cddd2ca3ec2dd58bbb519366a
Kaylee Simmons <kay@the-simmons.net>
2023-11-06 00:52:23 -0800

    Prevent spawning particles when jumping to a location instantly

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(not(test), windows_subsystem = "windows")]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 // Test naming occasionally uses camelCase with underscores to separate sections of
 // the test name.
 #![cfg_attr(test, allow(non_snake_case))]
