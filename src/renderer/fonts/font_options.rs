9a3bd650d3a8706ffcd4125d212cff1e15cd72f9
Fred Sundvik <fsundvik@gmail.com>
2024-04-24 11:54:48 +0300

    Change default hinting to slight
    Full hinting is a bit too aggressive, especially with fractional font sizes

--- src/renderer/fonts/font_options.rs
+++ src/renderer/fonts/font_options.rs
@@ -298,9 +298,9 @@ impl FontEdging {
 
 #[derive(Clone, Debug, Hash, PartialEq, Eq, Default)]
 pub enum FontHinting {
-    #[default]
     Full,
     Normal,
+    #[default]
     Slight,
     None,
 }
