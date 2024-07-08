44edd057c2f3f21e181b42a71fc3a0e17093e29b
Fred Sundvik <fsundvik@gmail.com>
2024-07-08 15:46:26 +0300

    Calculate the underline offset relative to the baseline offset

--- src/renderer/fonts/caching_shaper.rs
+++ src/renderer/fonts/caching_shaper.rs
@@ -212,7 +212,7 @@ impl CachingShaper {
 
     pub fn underline_position(&mut self) -> f32 {
         let metrics = self.metrics();
-        metrics.ascent - metrics.underline_offset + self.linespace / 2.
+        self.baseline_offset() - metrics.underline_offset
     }
 
     pub fn stroke_size(&mut self) -> f32 {
