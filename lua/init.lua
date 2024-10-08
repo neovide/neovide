ff96035395747533d6b44ac61ef568ed39084ace
Alexsander Falcucci <alex.falcucci@gmail.com>
2024-10-08 03:25:08 +0200

    chore: update touch pad functionality in lua script
    - delete default.profraw file
    - add touch_pad_module requirement to init.lua
    - create touch_pad_pressure.lua file with take_entity_under_cursor function

--- lua/init.lua
+++ lua/init.lua
@@ -10,6 +10,8 @@
 ---@type Args
 local args = ...
 
+local touch_pad_module = require("lua/modules/touch_pad_pressure")
+
 vim.g.neovide_channel_id = args.neovide_channel_id
 vim.g.neovide_version = args.neovide_version
 
@@ -67,42 +69,8 @@ vim.api.nvim_create_user_command("NeovideFocus", function()
     rpcnotify("neovide.focus_window")
 end, {})
 
-local function take_entity_under_cursor()
-    if vim.tbl_contains(vim.g.cursorword_disable_filetypes or {}, vim.bo.filetype) then
-        return
-    end
-
-    local mouse_pos = vim.fn.getmousepos()
-    local guifont = vim.api.nvim_get_option("guifont")
-    local column = vim.api.nvim_win_get_cursor(0)[2]
-    local cursor = vim.api.nvim_win_get_cursor(0)
-    local line = vim.api.nvim_get_current_line()
-
-    -- get the word under the cursor using matchstrpos
-    local curword, curword_start, _ = unpack(vim.fn.matchstrpos(line, [[\k*\%]] .. cursor[2] + 1 .. [[c\k*]]))
-
-    -- get screen position of the cursor
-    local screenpos = vim.fn.screenpos(mouse_pos.winid, cursor[1], cursor[2] + 1)
-
-    local entity
-    local entity_start
-    local url_pattern = "https?://[%w-_%.]+%.%w[%w-_%.%%%?%.:/+=&%%[%]#]*"
-    for url in line:gmatch(url_pattern) do
-        local s, e = line:find(url, 1, true)
-        -- check if the cursor is within this URL
-        if column >= s and column <= e then
-            entity = url
-            entity_start = s - 1
-        end
-    end
-
-    entity = entity or curword
-    entity_start = entity_start or curword_start
-    return entity, entity_start + 5, screenpos.row, guifont
-end
-
 vim.api.nvim_create_user_command("NeovideForceClick", function()
-    local cursorentity, entity_start, entity_end, guifont = take_entity_under_cursor()
+    local cursorentity, entity_start, entity_end, guifont = touch_pad_module.take_entity_under_cursor()
     rpcnotify("neovide.force_click", cursorentity, entity_start, entity_end, guifont)
 end, {})
 
---
+++ lua/modules/touch_pad_pressure.lua
@@ -0,0 +1,37 @@
+local M = {}
+
+M.take_entity_under_cursor = function()
+    if vim.tbl_contains(vim.g.cursorword_disable_filetypes or {}, vim.bo.filetype) then
+        return
+    end
+
+    local mouse_pos = vim.fn.getmousepos()
+    local guifont = vim.api.nvim_get_option("guifont")
+    local column = vim.api.nvim_win_get_cursor(0)[2]
+    local cursor = vim.api.nvim_win_get_cursor(0)
+    local line = vim.api.nvim_get_current_line()
+
+    -- get the word under the cursor using matchstrpos
+    local curword, curword_start, _ = unpack(vim.fn.matchstrpos(line, [[\k*\%]] .. cursor[2] + 1 .. [[c\k*]]))
+
+    -- get screen position of the cursor
+    local screenpos = vim.fn.screenpos(mouse_pos.winid, cursor[1], cursor[2] + 1)
+
+    local entity
+    local entity_start
+    local url_pattern = "https?://[%w-_%.]+%.%w[%w-_%.%%%?%.:/+=&%%[%]#]*"
+    for url in line:gmatch(url_pattern) do
+        local s, e = line:find(url, 1, true)
+        -- check if the cursor is within this URL
+        if column >= s and column <= e then
+            entity = url
+            entity_start = s - 1
+        end
+    end
+
+    entity = entity or curword
+    entity_start = entity_start or curword_start
+    return entity, entity_start + 5, screenpos.row, guifont
+end
+
+return M
