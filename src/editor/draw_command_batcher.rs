3075a94f2e86ad301ab45816baec87f0c61de748
fredizzimo <fsundvik@gmail.com>
2025-04-06 19:57:58 +0700

    feat: add an API for disabling redraw (#3097)
    * Add an api for temporarily disable redrawing
    
    * Update the documentation

--- src/editor/draw_command_batcher.rs
+++ src/editor/draw_command_batcher.rs
@@ -4,18 +4,39 @@ use winit::event_loop::EventLoopProxy;
 
 pub struct DrawCommandBatcher {
     batch: Vec<DrawCommand>,
+    enabled: bool,
+    queued: Vec<Vec<DrawCommand>>,
 }
 
 impl DrawCommandBatcher {
     pub fn new() -> DrawCommandBatcher {
-        Self { batch: Vec::new() }
+        Self {
+            batch: Vec::new(),
+            enabled: true,
+            queued: Vec::new(),
+        }
     }
 
     pub fn queue(&mut self, draw_command: DrawCommand) {
         self.batch.push(draw_command);
     }
 
+    pub fn set_enabled(&mut self, enabled: bool, proxy: &EventLoopProxy<UserEvent>) {
+        log::info!("Set redraw {enabled}");
+        if enabled && !self.enabled {
+            for queued in self.queued.drain(..) {
+                proxy.send_event(queued.into()).ok();
+            }
+        }
+        self.enabled = enabled;
+    }
+
     pub fn send_batch(&mut self, proxy: &EventLoopProxy<UserEvent>) {
-        proxy.send_event(self.batch.split_off(0).into()).ok();
+        let batch = self.batch.split_off(0);
+        if self.enabled {
+            proxy.send_event(batch.into()).ok();
+        } else {
+            self.queued.push(batch);
+        }
     }
 }
