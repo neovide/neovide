3ccb4a7e3aec200e9f9baa3a8c8b48e76f913d74
Fred Sundvik <fsundvik@gmail.com>
2025-10-15 22:33:24 +0300

    Move the application state to it's own struct for easier and safer cleanup

--- src/application.rs
+++ src/application.rs
@@ -18,14 +18,24 @@ use crate::{
     window::{ErrorWindow, NeovimWindow, UpdateLoop, UserEvent},
 };
 
-pub struct NeovideApplication {
-    pub exit_code: u8,
+struct ApplicationState {
     initial_config: Option<Config>,
-    current_window: Option<UpdateLoop>,
     proxy: EventLoopProxy<UserEvent>,
     settings: Arc<Settings>,
-    runtime: Option<NeovimRuntime>,
-    clipboard: Option<Arc<Mutex<Clipboard>>>,
+    // SAFETY: It's important that all rendering resources are cleaned up here before the EventLoop is destroyed
+    current_window: Option<UpdateLoop>,
+    // SAFETY: It's important that the runtime is cleaned up here, since it might contain references to the clipboard
+    // And that indirectly uses the event loop, so it has to be destroyed before that.
+    runtime: NeovimRuntime,
+    // SAFETY: It's important that the clipboard, which uses the event loop is deleted here
+    clipboard: Arc<Mutex<Clipboard>>,
+}
+
+pub struct NeovideApplication {
+    pub exit_code: u8,
+    // SAFETY: The application state is only valid until exiting has been called
+    // But that should be the last event that will ever be called
+    state: Option<ApplicationState>,
 }
 
 impl NeovideApplication {
@@ -35,22 +45,25 @@ impl NeovideApplication {
         settings: Arc<Settings>,
     ) -> Self {
         let clipboard = Arc::new(Mutex::new(Clipboard::new(event_loop)));
-        let runtime =
-            Some(NeovimRuntime::new(clipboard.clone()).expect("Failed to create runtime"));
+        let runtime = NeovimRuntime::new(clipboard.clone()).expect("Failed to create runtime");
         let proxy = event_loop.create_proxy();
-        Self {
-            exit_code: 0,
+        let state = Some(ApplicationState {
             initial_config: Some(initial_config),
             current_window: None,
             proxy,
             settings,
             runtime,
-            clipboard: Some(clipboard),
+            clipboard,
+        });
+        Self {
+            exit_code: 0,
+            state,
         }
     }
 
     fn schedule_next_event(&mut self, event_loop: &ActiveEventLoop) {
-        if let Some(window) = &self.current_window {
+        let state = self.state.as_mut().unwrap();
+        if let Some(window) = &state.current_window {
             event_loop.set_control_flow(ControlFlow::WaitUntil(window.get_event_deadline()));
         } else {
             event_loop.set_control_flow(ControlFlow::Wait);
@@ -59,6 +72,7 @@ impl NeovideApplication {
 
     fn handle_startup_errors(&mut self, err: anyhow::Error, event_loop: &ActiveEventLoop) {
         self.exit_code = 1;
+        let state = self.state.as_mut().unwrap();
         if stdout().is_terminal() {
             // The logger already writes to stderr
             log::error!("{}", &format_and_log_error_message(err));
@@ -67,14 +81,14 @@ impl NeovideApplication {
             let window = ErrorWindow::new(
                 format_and_log_error_message(err),
                 event_loop,
-                self.settings.clone(),
-                self.proxy.clone(),
-                self.clipboard.as_ref().unwrap().clone(),
+                state.settings.clone(),
+                state.proxy.clone(),
+                state.clipboard.clone(),
             );
-            self.current_window = Some(UpdateLoop::new(
+            state.current_window = Some(UpdateLoop::new(
                 Box::new(window),
-                self.proxy.clone(),
-                self.settings.clone(),
+                state.proxy.clone(),
+                state.settings.clone(),
             ));
         };
     }
@@ -88,7 +102,8 @@ impl ApplicationHandler<UserEvent> for NeovideApplication {
         event: winit::event::WindowEvent,
     ) {
         tracy_zone!("window_event");
-        if let Some(window) = &mut self.current_window {
+        let state = self.state.as_mut().unwrap();
+        if let Some(window) = &mut state.current_window {
             window.window_event(event_loop, window_id, event);
         }
         self.schedule_next_event(event_loop);
@@ -96,6 +111,7 @@ impl ApplicationHandler<UserEvent> for NeovideApplication {
 
     fn user_event(&mut self, event_loop: &ActiveEventLoop, event: UserEvent) {
         tracy_zone!("user_event");
+        let state = self.state.as_mut().unwrap();
         match event {
             UserEvent::LaunchFailure(err) => {
                 self.handle_startup_errors(err, event_loop);
@@ -104,7 +120,7 @@ impl ApplicationHandler<UserEvent> for NeovideApplication {
                 self.exit_code = code;
             }
             _ => {
-                if let Some(window) = &mut self.current_window {
+                if let Some(window) = &mut state.current_window {
                     window.user_event(event_loop, event);
                 }
             }
@@ -114,7 +130,8 @@ impl ApplicationHandler<UserEvent> for NeovideApplication {
 
     fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
         tracy_zone!("about_to_wait");
-        if let Some(window) = &mut self.current_window {
+        let state = self.state.as_mut().unwrap();
+        if let Some(window) = &mut state.current_window {
             window.about_to_wait(event_loop);
         }
         self.schedule_next_event(event_loop);
@@ -122,7 +139,8 @@ impl ApplicationHandler<UserEvent> for NeovideApplication {
 
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         tracy_zone!("resumed");
-        if let Some(window) = &mut self.current_window {
+        let state = self.state.as_mut().unwrap();
+        if let Some(window) = &mut state.current_window {
             window.resumed(event_loop);
         }
         self.schedule_next_event(event_loop);
@@ -130,31 +148,26 @@ impl ApplicationHandler<UserEvent> for NeovideApplication {
 
     fn exiting(&mut self, event_loop: &ActiveEventLoop) {
         tracy_zone!("exiting");
-        // SAFETY: It's important that all rendering resources are cleaned up here before the EventLoop is destroyed
-        self.current_window = None;
-
-        // SAFETY: It's important that the runtime is cleaned up here, since it might contain references to the clipboard
-        // And that indirectly uses the event loop, so it has to be destroyed before that.
-        self.runtime = None;
-        // SAFETY: It's important that the clipboard, which uses the event loop is deleted here
-        self.clipboard = None;
-        self.schedule_next_event(event_loop);
+        // SAFETY: It's important that all resources that use the EventLooped are cleaned up here
+        self.state = None;
+        event_loop.set_control_flow(ControlFlow::Wait);
     }
 
     fn new_events(&mut self, event_loop: &ActiveEventLoop, cause: StartCause) {
+        let state = self.state.as_mut().unwrap();
         if cause == StartCause::Init {
             tracy_zone!("init");
-            let config = self.initial_config.take().unwrap();
+            let config = state.initial_config.take().unwrap();
             let window = NeovimWindow::new(
                 config,
-                self.settings.clone(),
-                self.proxy.clone(),
-                self.runtime.as_mut().unwrap(),
+                state.settings.clone(),
+                state.proxy.clone(),
+                &mut state.runtime,
             );
-            self.current_window = Some(UpdateLoop::new(
+            state.current_window = Some(UpdateLoop::new(
                 Box::new(window),
-                self.proxy.clone(),
-                self.settings.clone(),
+                state.proxy.clone(),
+                state.settings.clone(),
             ));
             self.schedule_next_event(event_loop);
         }
