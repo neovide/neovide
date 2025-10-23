81493818b1f0610e617d523c6ad5966f72a32543
Akiyama Kohane <echo.aimless@gmail.com>
2025-10-23 11:06:17 +0800

    fix: the window should blur when either opacity or normal_opacity is less than 1.0

--- src/window/window_wrapper.rs
+++ src/window/window_wrapper.rs
@@ -490,6 +490,7 @@ impl WinitWindowWrapper {
             input_ime,
             theme,
             opacity,
+            normal_opacity,
             window_blurred,
             fullscreen,
             #[cfg(target_os = "macos")]
@@ -588,7 +589,7 @@ impl WinitWindowWrapper {
             self.renderer.grid_renderer.grid_scale
         );
 
-        window.set_blur(window_blurred && opacity < 1.0);
+        window.set_blur(window_blurred && opacity.min(normal_opacity) < 1.0);
 
         #[cfg(target_os = "windows")]
         if window_blurred {
