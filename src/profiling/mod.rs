ca5e41c0fbfa191dc7352cd2e13a4de5d63f1b38
Alexsander Falcucci <alex.falcucci@gmail.com>
2025-10-24 22:34:04 +0200

    feat: move profiling on windows platform - [10/20]
    - add a new module `profiling` to the `src/platform/windows/` directory
    - create a new file `profiling.rs` in the `src/platform/windows/` directory
    - refactor the `d3d` module in the `profiling` directory to be accessed from the `platform/windows` module

--- src/profiling/mod.rs
+++ src/profiling/mod.rs
@@ -1,10 +1,14 @@
+#[cfg(all(feature = "gpu_profiling", target_os = "windows"))]
+use crate::platform::windows;
 #[cfg(not(feature = "profiling"))]
 mod profiling_disabled;
 #[cfg(feature = "profiling")]
 mod profiling_enabled;
 
 #[cfg(all(feature = "gpu_profiling", target_os = "windows"))]
-pub mod d3d;
+pub mod d3d {
+    pub use crate::platform::windows::profiling::d3d::*;
+}
 #[cfg(feature = "gpu_profiling")]
 pub mod opengl;
 
