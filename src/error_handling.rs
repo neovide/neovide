51d3e36c9512d32b36a98b6c1d0201d18a5f7f2b
Alexsander Falcucci <alex.falcucci@gmail.com>
2025-12-01 18:39:53 +0100

    fix: ensure clean up during teardown and on drop (#3313)

--- src/error_handling.rs
+++ src/error_handling.rs
@@ -1,7 +1,7 @@
 use std::{
     io::{stdout, IsTerminal},
     process::ExitCode,
-    sync::Arc,
+    sync::{Arc, Mutex},
 };
 
 use anyhow::{Error, Result};
@@ -15,7 +15,7 @@ use crate::windows_attach_to_console;
 
 use crate::{
     bridge::{send_ui, ParallelCommand},
-    clipboard::ClipboardHandle,
+    clipboard::{Clipboard, ClipboardHandle},
     settings::Settings,
     window::{show_error_window, UserEvent},
 };
@@ -71,8 +71,10 @@ pub fn handle_startup_errors(
     err: Error,
     event_loop: EventLoop<UserEvent>,
     settings: Arc<Settings>,
-    clipboard: ClipboardHandle,
+    clipboard: Arc<Mutex<Clipboard>>,
 ) -> ExitCode {
+    let clipboard_handle = ClipboardHandle::new(&clipboard);
+
     // Command line output is always printed to the stdout/stderr
     if let Some(clap_error) = err.downcast_ref::<ClapError>() {
         #[cfg(target_os = "windows")]
@@ -88,7 +90,7 @@ pub fn handle_startup_errors(
             &format_and_log_error_message(err),
             event_loop,
             settings,
-            clipboard,
+            clipboard_handle,
         );
         ExitCode::from(1)
     }
