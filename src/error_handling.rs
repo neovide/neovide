3b5062625ae6572f310e7efb29fe767c0cc98bfe
Alexsander Falcucci <alex.falcucci@gmail.com>
2026-04-04 01:02:31 +0200

    fix: split startup preflight from runtime setup (#3453)

--- src/error_handling.rs
+++ src/error_handling.rs
@@ -20,6 +20,11 @@ use crate::{
     window::{EventPayload, show_error_window},
 };
 
+pub enum StartupErrorOutput {
+    Stderr,
+    Logger,
+}
+
 fn show_error(explanation: &str) -> ! {
     error!("{explanation}");
     panic!("{}", explanation.to_string());
@@ -69,6 +74,28 @@ This is the error that caused the crash. In case you don't know what to do with 
     msg
 }
 
+fn handle_clap_startup_error(err: &Error) -> Option<ExitCode> {
+    let clap_error = err.downcast_ref::<ClapError>()?;
+    #[cfg(target_os = "windows")]
+    windows_attach_to_console();
+    let _ = clap_error.print();
+    Some(ExitCode::from(clap_error.exit_code() as u8))
+}
+
+pub fn report_startup_error(err: Error, output: StartupErrorOutput) -> ExitCode {
+    if let Some(exit_code) = handle_clap_startup_error(&err) {
+        return exit_code;
+    }
+
+    let msg = format_and_log_error_message(err);
+    match output {
+        StartupErrorOutput::Stderr => eprintln!("{msg}"),
+        StartupErrorOutput::Logger => log::error!("{msg}"),
+    }
+
+    ExitCode::from(1)
+}
+
 pub fn handle_startup_errors(
     err: Error,
     event_loop: EventLoop<EventPayload>,
@@ -76,15 +103,11 @@ pub fn handle_startup_errors(
     clipboard: Arc<Mutex<Clipboard>>,
 ) -> ExitCode {
     // Command line output is always printed to the stdout/stderr
-    if let Some(clap_error) = err.downcast_ref::<ClapError>() {
-        #[cfg(target_os = "windows")]
-        windows_attach_to_console();
-        let _ = clap_error.print();
-        ExitCode::from(clap_error.exit_code() as u8)
+    if let Some(exit_code) = handle_clap_startup_error(&err) {
+        exit_code
     } else if stdout().is_terminal() {
         // The logger already writes to stderr
-        log::error!("{}", &format_and_log_error_message(err));
-        ExitCode::from(1)
+        report_startup_error(err, StartupErrorOutput::Logger)
     } else {
         show_error_window(&format_and_log_error_message(err), event_loop, settings, clipboard);
         ExitCode::from(1)
